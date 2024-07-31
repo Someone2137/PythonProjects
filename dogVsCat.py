981356612d5d8b347c12876e50bcaeef444d7f02
Someone <martynaogrodniczak@gmail.com>
2024-07-31 11:02:22 +0200

    Update

--- dogVsCat.py
+++ dogVsCat.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_excel(r"C:\Users\marty\OneDrive\Pulpit\Programowanie\dogVsCat.xlsx")
+data = pd.read_excel(r"C:\Users\marty\OneDrive\Pulpit\Programowanie\PythonProjects\dogVsCat.xlsx")
 Country = data["Country"]
 Cat = data["cat"]
 
-plt.figure(figsize=(10, 5))
-plt.bar(Country, Cat, color="lightblue")
+plt.figure(figsize=(10, 7))
+bars = plt.bar(Country, Cat, color="lightblue")
+
+for bar in bars:
+    yValue = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width()/2, yValue, f'{yValue}%', ha='center', va='bottom')
+
 plt.xlabel("Country", labelpad=15)
 plt.ylabel("Percentage", labelpad=15)
 plt.title("Pet owners that said they have a cat in 2022")
