cf90a64f1e181d0b4db5af3f87a80e2b0759402c
Someone <martynaogrodniczak@gmail.com>
2024-08-09 10:40:44 +0200

    Update

--- Tables.py
+++ Tables.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-data = pd.read_csv(r"C:\Users\marty\PycharmProjects\data.csv", header=None, names=["Sex", "Variable1", "Variable2", "Variable3", "Variable4", "Variable5", "Variable6", "Variable7", "Variable8"])
+data = pd.read_csv(r"C:\Users\marty\PycharmProjects\data.csv", names=["Sex", "Length", "Diameter", "Height", "Whole weight", "Shucked weight", "Viscera weight", "Shell weight", "Rings"])
 
 sexCount = data["Sex"].value_counts()
 sexPercentage = data["Sex"].value_counts(normalize=True) * 100
@@ -17,7 +18,7 @@ result.index = ["Male", "Female", "Infant"]
 
 print(result, "\n")
 
-quantativeValues = ["Variable1", "Variable2", "Variable3", "Variable4", "Variable5", "Variable6", "Variable7", "Variable8"]
+quantativeValues = ["Length", "Diameter", "Height", "Whole weight", "Shucked weight", "Viscera weight", "Shell weight", "Rings"]
 quantativeTable = data[quantativeValues]
 
 summaryTable = pd.DataFrame(
@@ -33,3 +34,16 @@ summaryTable = pd.DataFrame(
 )
 
 print(summaryTable)
+
+plt.figure(figsize=[10,7])
+bars = sexCount.plot(kind="bar", color=["blue", "green", "yellow"])
+plt.title("Counts of ocurrances of each sex")
+plt.xlabel("Sex", labelpad=15)
+plt.ylabel("Count", labelpad=15)
+bars.set_xticklabels(["Male", "Female", "Infant"], rotation=1)
+
+for bar in bars.patches:
+    yValue = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width()/ 2, yValue, f"{yValue}", ha="center", va="bottom")
+
+plt.show()
